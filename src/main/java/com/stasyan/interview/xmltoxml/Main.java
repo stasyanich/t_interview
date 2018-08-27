b3d9fe94b6a34fa500fd11ad4aefcfc5c1c4b72d
Stanislav Mosyagin <mosstnslv@gmail.com>
2018-08-27 18:16:41 +0500

    DBUtilStatic.java: добавил addRecord, настроил пакетную запись; Main.java встроил подсчет времени выполения;

--- src/main/java/com/stasyan/interview/xmltoxml/Main.java
+++ src/main/java/com/stasyan/interview/xmltoxml/Main.java
@@ -1,22 +1,45 @@
 package com.stasyan.interview.xmltoxml;
 
+import com.stasyan.interview.xmltoxml.util.DBUtilStatic;
+
 public class Main {
 
 
     private static String firstFileName = "1";
     private static String secondFileName = "2";
+    private static int bigN = 1;
 
+    // данные для подключения к бд: адрес, логин, пароль?
     public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
 
-        System.out.println("fist key firstFileName and secondFileName");
-        if(args.length>2) {
+        if (args.length==3){
             firstFileName = args[0];
             secondFileName = args[1];
+            bigN = Integer.getInteger(args[2]);
+        }else if(args.length==2) {
+            firstFileName = args[0];
+            secondFileName = args[1];
+        }else if (args.length==1){
+            bigN = Integer.getInteger(args[0]);
+        }else if (args.length<1){
+            //NOP
         }else {
-            System.out.println("принимаемых пораметров должно быть 2 и более");
+            System.out.println("Ошибка в параметрах:" + args.toString());
         }
 
-        System.out.println(firstFileName + ".xml " + secondFileName + ".xml");
+//        System.out.println(firstFileName + ".xml " + secondFileName + ".xml" + " записалось: " + bigN + " раз");
+
+        new DBUtilStatic();
+
+        doIt(firstFileName,secondFileName,bigN);
+
+        long timeSpend = System.currentTimeMillis()-startTime;
+        System.out.println("программа выполнялась: "+timeSpend/1000L );
+    }
+
+    private static void doIt(String firstFileName, String secondFileName, int bigN) {
+
     }
 
 }
--- src/main/java/com/stasyan/interview/xmltoxml/util/DBUtilStatic.java
+++ src/main/java/com/stasyan/interview/xmltoxml/util/DBUtilStatic.java
@@ -1,31 +1,39 @@
 package com.stasyan.interview.xmltoxml.util;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import com.sun.istack.internal.NotNull;
+
+import java.sql.*;
 
 public class DBUtilStatic {
     private static final String JDBC_DRIVER = "org.sqlite.JDBC";
     private static final String CONN_STR = "jdbc:sqlite:";
+    private static Connection connection;
 
     static {
         try {
             Class.forName(JDBC_DRIVER);
+            connection = getConnection();
+            connection.setAutoCommit(false);
+
+            clearDB();
         }catch (ClassNotFoundException e){
             System.err.println("Enable to load jdbc driver");
+        }catch (XmlToXmlException e){
+            System.err.println(e.getMessage());
+        }catch (SQLException e){
+            System.err.println(e.getMessage());
         }
     }
 
     public DBUtilStatic() {
         try {
-            createSqlDB(getConnection());
-        }catch (XmlToXmlException e){
-            System.err.println(e.getMessage());
+            addRecord(1_000_000);
+        } catch (XmlToXmlException e) {
+            e.printStackTrace();
         }
     }
 
-    public static Connection getConnection() throws XmlToXmlException{
+    private static Connection getConnection() throws XmlToXmlException{
         try {
             return DriverManager.getConnection(CONN_STR+"xmltoxml.db");
         }catch (SQLException e){
@@ -34,12 +42,10 @@ public class DBUtilStatic {
     }
 
 
-    public static void createSqlDB(Connection connection) throws XmlToXmlException {
-        String queryToDropDB = "DROP TABLE IF EXISTS TEST;";
-        String queryToCreateDB = "CREATE TABLE TEST(FIELD INTEGER);";
+    private static void querySqlDB(String query) throws XmlToXmlException {
 
         try(Statement statement = connection.createStatement()) {
-            statement.execute(queryToDropDB + queryToCreateDB);
+            statement.execute(query);
 
         }catch (SQLException e){
             throw new XmlToXmlException("Невозможно получить statement" + e);
@@ -49,4 +55,24 @@ public class DBUtilStatic {
         }
     }
 
+    private static void addRecord(int amount) throws XmlToXmlException{
+        String sqlQuery = "INSERT INTO TEST (FIELD) VALUES (?)";
+        try(PreparedStatement statement = connection.prepareStatement(sqlQuery)){
+
+            for (int i=1;i<=amount;i++){
+                statement.setInt(1, i);
+                statement.addBatch();
+            }
+
+            statement.executeBatch();
+            connection.commit();
+        }catch (SQLException e){
+            throw new XmlToXmlException(e);
+        }
+    }
+
+    private static void clearDB() throws XmlToXmlException{
+        querySqlDB("DROP TABLE IF EXISTS TEST");
+        querySqlDB("CREATE TABLE TEST (FIELD INTEGER)");
+    }
 }
