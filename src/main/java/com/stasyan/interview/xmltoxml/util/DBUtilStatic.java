6fedd27064099ef584e6508e1230824df02956fc
Stanislav Mosyagin <mosstnslv@gmail.com>
2018-08-27 15:50:31 +0500

    добавил XmlToXmlException.java; DBUtilStatic.java: добавил createSqlDB:

--- src/main/java/com/stasyan/interview/xmltoxml/util/DBUtilStatic.java
+++ src/main/java/com/stasyan/interview/xmltoxml/util/DBUtilStatic.java
@@ -3,6 +3,7 @@ package com.stasyan.interview.xmltoxml.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DBUtilStatic {
     private static final String JDBC_DRIVER = "org.sqlite.JDBC";
@@ -14,18 +15,38 @@ public class DBUtilStatic {
         }catch (ClassNotFoundException e){
             System.err.println("Enable to load jdbc driver");
         }
-
     }
 
-    public static Connection getConnection(){
-
-        try(Connection connection = DriverManager.getConnection(CONN_STR+"xmltoxml.db")) {
-            return connection;
-        }catch (SQLException e){
-            System.err.println("Подключение к BD невозможно");
-            return null;
+    public DBUtilStatic() {
+        try {
+            createSqlDB(getConnection());
+        }catch (XmlToXmlException e){
+            System.err.println(e.getMessage());
         }
+    }
 
+    public static Connection getConnection() throws XmlToXmlException{
+        try {
+            return DriverManager.getConnection(CONN_STR+"xmltoxml.db");
+        }catch (SQLException e){
+            throw new XmlToXmlException(e);
+        }
+    }
+
+
+    public static void createSqlDB(Connection connection) throws XmlToXmlException {
+        String queryToDropDB = "DROP TABLE IF EXISTS TEST;";
+        String queryToCreateDB = "CREATE TABLE TEST(FIELD INTEGER);";
+
+        try(Statement statement = connection.createStatement()) {
+            statement.execute(queryToDropDB + queryToCreateDB);
+
+        }catch (SQLException e){
+            throw new XmlToXmlException("Невозможно получить statement" + e);
+
+        }catch (NullPointerException e){
+            throw new XmlToXmlException("Невозможно получить statement: NPE" + e);
+        }
     }
 
 }
---
+++ src/main/java/com/stasyan/interview/xmltoxml/util/XmlToXmlException.java
@@ -0,0 +1,11 @@
+package com.stasyan.interview.xmltoxml.util;
+
+public class XmlToXmlException extends Exception {
+    public XmlToXmlException(String message) {
+        super(message);
+    }
+
+    public XmlToXmlException(Throwable cause) {
+        super(cause);
+    }
+}
