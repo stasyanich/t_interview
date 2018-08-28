146a635412ea8a84ab238f3778d1d5b3446fadc0
Stanislav Mosyagin <mosstnslv@gmail.com>
2018-08-28 18:46:59 +0500

    Main.java добавил методы saveObjectsToXml, readObjectsFromXml

--- src/main/java/com/stasyan/interview/xmltoxml/Main.java
+++ src/main/java/com/stasyan/interview/xmltoxml/Main.java
@@ -15,7 +15,7 @@ public class Main {
     private static String firstFileName = "1";
     private static String secondFileName = "2";
     private static String postfix = ".xml";
-    private static int bigN = 1_000;
+    private static int bigN = 1_00;
 
     // данные для подключения к бд: адрес, логин, пароль?
     public static void main(String[] args) {
@@ -25,18 +25,6 @@ public class Main {
 
         doIt(firstFileName, secondFileName, bigN);
 
-        try {
-            XmlEntries xmlEntries = new XmlEntries();
-           ArrayList<XmlEntry> entries = DBUtilStatic.readRecordsFromDB();
-            xmlEntries.setXmlEntries(entries);
-            JaxbParser jaxbParser = new JaxbParser();
-
-            jaxbParser.saveObject(new File(firstFileName+postfix), xmlEntries);
-        } catch (XmlToXmlException e) {
-            e.printStackTrace();
-        }catch (JAXBException e){
-            e.printStackTrace();
-        }
         long timeSpend = System.currentTimeMillis() - startTime;
         System.err.println("программа выполнялась: " + timeSpend / 1000L + " секунд");
         System.err.println(firstFileName + ".xml " + secondFileName + ".xml" + " записалось: " + bigN + " раз");
@@ -47,9 +35,30 @@ public class Main {
     private static void doIt(String firstFileName, String secondFileName, int bigN) {
         try {
             DBUtilStatic.addRecord(bigN);
+            ArrayList<XmlEntry> entries = DBUtilStatic.readRecordsFromDB();
+            saveObjectsToXml(entries,firstFileName+postfix);
+            readObjectsFromXml(firstFileName+postfix);
         } catch (XmlToXmlException e) {
             e.printStackTrace();
+        }catch (JAXBException j){
+            j.printStackTrace();
         }
+    }
+
+    private static void saveObjectsToXml(ArrayList<XmlEntry> arrayList, String fileName) throws JAXBException{
+        XmlEntries xmlEntries = new XmlEntries();
+
+        xmlEntries.setXmlEntries(arrayList);
+        JaxbParser jaxbParser = new JaxbParser();
+
+        jaxbParser.saveObject(new File(fileName), xmlEntries);
+    }
+
+    private static XmlEntries readObjectsFromXml(String filename) throws JAXBException{
+        JaxbParser jaxbParser = new JaxbParser();
+        ArrayList<XmlEntry> result;
+        XmlEntries xmlEntries =(XmlEntries) jaxbParser.getObject(new File(filename), XmlEntries.class);
+        return xmlEntries;
     }
 
     private static void incomingParameters(String[] args){
